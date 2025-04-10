a28984416d320694174d5c753a7577d3081c995d
chpurnabhargav <chpurnabhargav@gmail.com>
2025-04-10 09:42:50 +0530

    all3

--- frontend/pages/Card.js
+++ frontend/pages/Card.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardHeader, CardTitle, CardContent } from '../../components/ui/card';
+import { Card, CardHeader, CardTitle, CardContent } from '../components/ui/card';
+
 
 const CardPage = () => {
   return (
